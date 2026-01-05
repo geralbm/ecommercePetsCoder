59c149932cbc3e77f685d680def5194c7a1b33e0
Geraldina <geralbm@gmail.com>
2026-01-05 19:38:32 -0300

    ComponenteFooter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import '../css/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <ul className = 'footer-caja'>
+                <li className = 'anchor-footer'><a href="#">CONTACTANOS</a></li>
+                <li className = 'anchor-footer'><a href="#">SUCURSALES</a></li>
+            </ul>
+
+            
+
+            <div className='copyR'>
+                <p>2025 - Pets Love</p>
+            </div>
+        </div>
+    )
+}
+export default Footer;
