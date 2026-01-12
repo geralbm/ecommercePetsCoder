f38a40a524abb9304599b739a6778fb589f184e8
Geraldina <geralbm@gmail.com>
2026-01-12 18:42:52 -0300

    HookUseState-ItemCount

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,6 +1,7 @@
 import '../css/CardHomes.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ItemCount from './ItemCount';
 
 
 function CardHome () {
@@ -9,20 +10,24 @@ function CardHome () {
       <Card.Img variant="top" src="../public/imagenesCards/CardHome1.jpg" alt='Card Home' />
       <Card.Body>
         <Card.Title style={{color:'brown'}}>CAMA MOISES ARTESANAL</Card.Title>
-        <Button  className= 'button-home' variant="danger">Agregar al Carrito</Button>
+        <ItemCount stock={10}/>
+        <Button  className= 'd-block mx-auto' variant="danger">Agregar al Carrito</Button>
       </Card.Body>
 
+
+      <Card.Img variant="top" src="../public/imagenesCards/CardHome1.jpg" alt='Card Home' />
+      <Card.Body>
+        <Card.Title style={{color:'brown'}}>CAMA MOISES ARTESANAL</Card.Title>
+        <p></p>
+        <ItemCount stock={10}/>
+        <Button  className= 'd-block mx-auto' variant="danger">Agregar al Carrito</Button>
+      </Card.Body>
 
  <Card.Img variant="top" src="../public/imagenesCards/CardHome1.jpg" alt='Card Home' />
       <Card.Body>
         <Card.Title style={{color:'brown'}}>CAMA MOISES ARTESANAL</Card.Title>
-        <Button  className= 'button-home' variant="danger">Agregar al Carrito</Button>
-      </Card.Body>
-
- <Card.Img variant="top" src="../public/imagenesCards/CardHome1.jpg" alt='Card Home' />
-      <Card.Body>
-        <Card.Title style={{color:'brown'}}>CAMA MOISES ARTESANAL</Card.Title>
-        <Button  className= 'button-home' variant="danger">Agregar al Carrito</Button>
+        <ItemCount stock={10}/>
+        <Button  className= 'd-block mx-auto' variant="danger">Agregar al Carrito</Button>
       </Card.Body>
 
 
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,35 @@
+//Uso de Hook -  USE STATE REACT
+import { useState} from "react";
+import Badge from 'react-bootstrap/Badge';
+import Stack from 'react-bootstrap/Stack';
+
+
+//Declaracion de Hook
+const ItemCount = ({stock}) => {
+    const [counter, setCounter] = useState(0)
+    
+    
+    //Funcion que suma en el contador de productos
+    const sumar = ()=>{
+        if (counter < stock){
+            setCounter(counter + 1) 
+        }
+    }
+
+    //Funcion que resta el contador de productos
+    const restar = () =>{
+        if (counter > 0){
+            setCounter(counter - 1)
+        }
+    }
+    
+    return (
+        <div className="w-50 mx-auto bg-light p-3 d-flex justify-content-center align-items-center">
+            <Badge bg="danger"  onClick={restar}>-</Badge>
+            <Badge bg="secondary">{counter}</Badge>
+            <Badge bg="success" onClick={sumar}>+</Badge>
+            
+        </div>
+    )
+}
+export default ItemCount;
